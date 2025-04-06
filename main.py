e87d3ec3bce6d0e4919714cf9d3c87d624ef47ae
Keshav Aher <keshavaher777@gmail.com>
2025-04-06 16:02:06 +0530

    data validation complleted

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline                                                      
 
 logger.info("Welcome to our cutome logging data science project")
+
+
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>>>>> Stage {STAGE_NAME} stated>>>>>>>>>>>")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>>>> Stage {STAGE_NAME} completed >>>>><<<<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Data Validation Stage"
+
+try:   
+    logger.info(f">>>>>>>>>> Stage {STAGE_NAME} stated>>>>>>>>>>>")
+    data_ingestion=DataValidationTrainingPipeline()
+    data_ingestion.initiate_data_validation()
+    logger.info(f">>>>>>>> Stage {STAGE_NAME} completed >>>>><<<<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
