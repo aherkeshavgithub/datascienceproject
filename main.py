5758af89cd25f652ce48776041c2e893ef431d98
Keshav Aher <keshavaher777@gmail.com>
2025-04-08 19:02:37 +0530

    model trainer implementation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline                                                      
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline 
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 logger.info("Welcome to our cutome logging data science project")
 
@@ -34,6 +35,17 @@ try:
     logger.info(f">>>>>>>>>> Stage {STAGE_NAME} stated>>>>>>>>>>>")
     data_ingestion=DataTransformationTrainingPipeline()
     data_ingestion.initiate_data_transformation()
+    logger.info(f">>>>>>>> Stage {STAGE_NAME} completed >>>>><<<<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+    logger.info(f">>>>>>>>>> Stage {STAGE_NAME} stated>>>>>>>>>>>")
+    data_ingestion=ModelTrainerTrainingPipeline()
+    data_ingestion.initiate_model_training()
     logger.info(f">>>>>>>> Stage {STAGE_NAME} completed >>>>><<<<<<<\n\nx===========x")
 except Exception as e:
     logger.exception(e)
